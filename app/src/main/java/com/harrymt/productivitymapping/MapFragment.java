b36028c56b33b3d654264d5dc91bb5baff787e8d
Harry Mumford-Turner <harrymt@me.com>
2016-02-08 17:06:13 +0000

    Fixed nested fragment issue
    Was defining the map and dynamically creating it, now we just add it to
    the frame layout.
    Added additional logging so it is easier to see hone the map fragment
    is changing state.

--- app/src/main/java/com/harrymt/productivitymapping/MapFragment.java
+++ app/src/main/java/com/harrymt/productivitymapping/MapFragment.java
@@ -3,6 +3,7 @@ package com.harrymt.productivitymapping;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,11 +19,13 @@ public class MapFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        Log.d("g53ids", "MapFragment.onCreateView()");
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
+        Log.d("g53ids", "MapFragment.onActivityCreated()");
         super.onActivityCreated(savedInstanceState);
         FragmentManager fm = getChildFragmentManager();
         fragment = (SupportMapFragment) fm.findFragmentById(R.id.map);
@@ -34,10 +37,21 @@ public class MapFragment extends Fragment {
 
     @Override
     public void onResume() {
+        Log.d("g53ids", "MapFragment.onResume()");
         super.onResume();
         if (map == null) {
             map = fragment.getMap();
             map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
         }
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d("g53ids", "MapFragment.onDestroy()");
+    }
+
+    @Override
+    public void onPause() {
+        Log.d("g53ids", "MapFragment.onPause()");
     }
 }
