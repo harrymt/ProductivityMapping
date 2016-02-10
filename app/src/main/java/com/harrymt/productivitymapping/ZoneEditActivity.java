dcef6800de4c139890188b6062ad0a15e7769860
Harry Mumford-Turner <harrymt@me.com>
2016-02-10 18:20:18 +0000

    Added map to zone edit activity

--- app/src/main/java/com/harrymt/productivitymapping/ZoneEditActivity.java
+++ app/src/main/java/com/harrymt/productivitymapping/ZoneEditActivity.java
@@ -4,13 +4,24 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class ZoneEditActivity extends Activity {
+public class ZoneEditActivity extends FragmentActivity implements OnMapReadyCallback  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_zone_edit);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     public void setZoneCoords(View view) {
@@ -18,5 +29,17 @@ public class ZoneEditActivity extends Activity {
         Intent zoneCoordinates = new Intent();
         setResult(RESULT_CODE, zoneCoordinates);
         finish();
+    }
+
+    private GoogleMap mMap;
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney, Australia, and move the camera.
+        LatLng sydney = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
