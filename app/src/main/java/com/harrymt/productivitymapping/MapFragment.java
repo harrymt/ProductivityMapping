e90fe93859ba6285079f59c3376ec8fc6486f7ac
Harry Mumford-Turner <harrymt@me.com>
2016-02-15 15:25:52 +0000

    Update Map for demo

--- app/src/main/java/com/harrymt/productivitymapping/MapFragment.java
+++ app/src/main/java/com/harrymt/productivitymapping/MapFragment.java
@@ -14,7 +14,9 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
@@ -38,26 +40,60 @@ public class MapFragment extends Fragment  implements OnMapReadyCallback {
         mMap.setIndoorEnabled(false);
         setZoneLatLngs();
 
-        enableCurrentLocation();
+        Marker m = mMap.addMarker(new MarkerOptions().position(new LatLng(52.9536037, -1.1890631)).title("Home: 70%"));
+        m.showInfoWindow();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(52.9536037, -1.1890631), 18.0f));
+    }
+
+//    private void setZoneLatLngs() {
+//        // Add a marker and move the camera.
+//        LatLng location = new LatLng(52.9487713,-1.17);
+//
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(52.9505425,-1.1706994)).title("Marker at home"));
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(52.9205425,-1.1706994)).title("Marker at home"));
+//        mMap.addMarker(new MarkerOptions().position(new LatLng(-52.9105425,-1.1706994)).title("Marker at home"));
+//
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+//    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d("g53ids", "TrackFragment.onPause()");
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+
+    public Zone[] getZones() {
+
+        return new Zone[] {
+                new Zone(52.9536037,-1.1890631, 10.0),
+                new Zone(52.9205425,-1.17, 100.0),
+                new Zone(52.9417713,-1.17, 100.0),
+                new Zone(52.9387713,-1.17, 100.0)
+        };
     }
 
     private void setZoneLatLngs() {
-        // Add a marker and move the camera.
-        LatLng location = new LatLng(52.9487713,-1.17);
-
-        mMap.addMarker(new MarkerOptions().position(new LatLng(52.9505425,-1.1706994)).title("Marker at home"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(52.9205425,-1.1706994)).title("Marker at home"));
-        mMap.addMarker(new MarkerOptions().position(new LatLng(-52.9105425,-1.1706994)).title("Marker at home"));
-
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+        Zone[] zones = getZones();
+        for(Zone zone : zones) {
+            drawCircle(zone);
+        }
     }
 
-    private void enableCurrentLocation() {
-        if (ContextCompat.checkSelfPermission(this.getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
-                == PackageManager.PERMISSION_GRANTED) {
-            mMap.setMyLocationEnabled(true);
-        } else {
-            // Show rationale and request permission.
-        }
+    private void drawCircle(Zone zone) {
+        int strokeColor = 0xffff0000; //red outline
+        int shadeColor = 0x44ff0000; //opaque red fill
+
+        CircleOptions circleOptions = new CircleOptions()
+                .center(new LatLng(zone.lat, zone.lng))
+                .radius(zone.radiusInMeters)
+                .fillColor(shadeColor)
+                .strokeColor(strokeColor)
+                .strokeWidth(8);
+        mMap.addCircle(circleOptions);
     }
 }
